086336aa4cfa1a179a10c20497fc7691832b85c8
Tim Chepeleff <tim.chepeleff@gmail.com>
2015-08-01 21:35:45 -0400

    masterminder file structure

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Backend::Application.config.secret_token = 'b6c3d2ecbc2c2db8631a84928b05528da514ed4723ecbc27ebf809349b23fcc480c9887894bdbba28bd6135286c517031abd733efc0ca75f5cdcca6ee464aeb5'
