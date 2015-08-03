2c9060150e84f72c7e90199b2eb9c55b23f85d30
Tim Chepeleff <tim.chepeleff@gmail.com>
2015-08-03 17:26:49 -0400

    working nested relationship (categories/tasks)

--- frontend/app/routes/categories/tasks/new.js
+++ frontend/app/routes/categories/tasks/new.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params){
-    return this.store.createRecord('task');
+  model: function(){
+    return this.store.createRecord('task', {
+      state: 'borrowed',
+      category:this.modelFor('categories/show')
+    });
   },
   actions: {
     save(task) {
